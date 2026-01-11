e37e8d49f131eab839535915af1b191cbf876787
SHANJAY975 <shanjay.ssj@gmail.com>
2026-01-11 20:29:19 +0530

    Junit Test Basics

---
+++ unit-testing-demo/src/main/java/com/ust/app/Main.java
@@ -0,0 +1,9 @@
+package com.ust.app;
+
+public class Main {
+    static void main() {
+        Calculator cal = new Calculator();
+        var x = cal.add(1,9);
+        System.out.println(x);
+    }
+}
