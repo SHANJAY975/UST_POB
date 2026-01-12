c8d58627d3eecc80c0627f84ee92980468189783
SHANJAY975 <shanjay.ssj@gmail.com>
2026-01-12 19:01:52 +0530

    Unit testing done

--- unit-testing-demo/src/test/java/com/ust/app/CalculatorTest.java
+++ unit-testing-demo/src/test/java/com/ust/app/CalculatorTest.java
@@ -1,11 +1,25 @@
 package com.ust.app;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-    Calculator calc = new Calculator();
+    Calculator calc;
+
+    @BeforeEach
+    void setUp(){
+        calc = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown(){
+        calc = null;
+    }
 
     @Test
     void testAdd(){
@@ -15,10 +29,19 @@ class CalculatorTest {
     }
 
     @Test
+    @EnabledOnOs(OS.WINDOWS)
+    // Helo to run on Particular os
     void testMultiply(){
         int a = 10;
         int b = 20;
         assertEquals(200, calc.multiply(a,b));
+    }
+
+    @Test
+    void testDivide(){
+        assertEquals(10,calc.divide(100,10));
+        assertThrows(ArithmeticException.class, ()->calc.divide(10,0));
+
     }
 
 
